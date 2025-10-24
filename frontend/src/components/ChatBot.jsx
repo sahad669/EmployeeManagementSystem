dcb50f956906d483513d511e97fc8988cc98429f
sahad <sahadmosco@gmail.com>
2025-10-25 03:26:19 +0400

    chatbot set button fixed right

--- frontend/src/components/ChatBot.jsx
+++ frontend/src/components/ChatBot.jsx
@@ -11,8 +11,8 @@ const ChatBot = ({ isOpen, onClose }) => {
     setMessage("");
 
     try {
-      const res = await axios.post("http://localhost:4000/api/chat", { message });
-      setChat([...newChat, { sender: "bot", text: ( res).data.reply }]);
+      const res = axios.post("http://localhost:4000/api/chat", { message });
+      setChat([...newChat, { sender: "bot", text: (await res).data.reply }]);
     } catch {
       setChat([...newChat, { sender: "bot", text: "error connecting" }]);
     }
